fc9766aa38c1bf1bdeb5af0b21aafc6ab3bb1aab
Joao Bueno <joaobueno121212@gmail.com>
2024-12-03 12:15:46 -0300

    simulating pagination

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,10 +5,25 @@ import { loadPosts } from "../../utils/loadPosts";
 
 function Home() {
     const [allposts, setAllPosts] = useState([]);
+    const [posts, setPosts] = useState([]);
+    const [page, setPage] = useState(0);
+    const [postPerPage, setPostPerPage] = useState(5);
+    const [noMorePosts, setNoMorePosts] = useState(false);
 
     const loadPostsData = async () => {
         const data = await loadPosts();
         setAllPosts(data);
+        setPosts(data.slice(page, page+postPerPage));
+    }
+
+    const loadMorePosts = () => {
+        const nextPage = page + postPerPage;
+        setPage(nextPage);
+        setPosts(p => [...p, ...allposts.slice(nextPage, nextPage+postPerPage)]);
+
+        if((nextPage + postPerPage) >= allposts.length){
+            setNoMorePosts(true)
+        }
     }
 
     useEffect(() => {
@@ -18,7 +33,11 @@ function Home() {
     return (
         <div>
             <section className="container">
-                <Posts allposts={allposts}/>
+                <Posts allposts={posts}/>
+                <button className="button-load-posts"
+                    onClick={loadMorePosts}
+                    disabled={noMorePosts}
+                >Load more posts</button>
             </section>
         </div>
     );
